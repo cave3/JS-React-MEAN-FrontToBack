02d811601a89eb4d48f12426486173d1de8bb9fc
wrk <wrks@mac.com>
2018-10-31 19:04:32 +0100

    added User registration, login, JWT token creation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://cave3:cave3dev@ds147033.mlab.com:47033/cave3-devconnector'
+    'mongodb://cave3:cave3dev@ds147033.mlab.com:47033/cave3-devconnector',
+  secretOrKey: 'secret'
 };
